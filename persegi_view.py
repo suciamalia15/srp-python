36b8c54a827f20268c67aaf20d99defdfd8c84ba
raudatul18 <raudatulmusyrofahmaulaisjati@gmail.com>
2022-05-24 08:46:51 +0700

    siap

---
+++ persegi_view.py
@@ -0,0 +1,11 @@
+from persegi import persegi
+from persegi_controller import persegi_controller
+
+
+class persegiview:
+
+    def show_luas(self, persegi: persegi, persegi_controller: persegicontroller)
+        print (persegi_controller.hitung_luas(persegi))
+
+    def show_keliling(self, persegi: persegi, persegi_controller:persegicontroller)
+        print (persegi_controller.hitung_keliling(persegi))
